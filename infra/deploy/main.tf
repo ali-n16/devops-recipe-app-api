c2b09b5ad0c673578f0a59275945570c5e48594a
ali-n16 <ali_r91@hotmail.com>
2025-09-23 13:39:13 +0100

    db

--- infra/deploy/main.tf
+++ infra/deploy/main.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket               = "alin1611-backend"
-    key                  = "myproject/dev/terraform.tfstate"
+    key                  = "tfstate"
     workspace_key_prefix = "tf-state-deploy-env"
     region               = "eu-west-1"
     encrypt              = true
