4665e8049f1214b55b3908df7e1508f6e8b8255e
ali-n16 <73839888+ali-n16@users.noreply.github.com>
2025-09-23 10:20:26 +0100

    Update iam.tf

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -51,3 +51,146 @@ resource "aws_iam_user_policy_attachment" "tf_backend" {
   user       = aws_iam_user.cd.name
   policy_arn = aws_iam_policy.tf_backend.arn
 }
+
+# #########################
+# # Policy for ECR access #
+# #########################
+
+# data "aws_iam_policy_document" "ecr" {
+#   statement {
+#     effect    = "Allow"
+#     actions   = ["ecr:GetAuthorizationToken"]
+#     resources = ["*"]
+#   }
+
+#   statement {
+#     effect = "Allow"
+#     actions = [
+#       "ecr:CompleteLayerUpload",
+#       "ecr:UploadLayerPart",
+#       "ecr:InitiateLayerUpload",
+#       "ecr:BatchCheckLayerAvailability",
+#       "ecr:PutImage"
+#     ]
+#     resources = [
+#       aws_ecr_repository.app.arn,
+#       aws_ecr_repository.proxy.arn,
+#     ]
+#   }
+# }
+
+# resource "aws_iam_policy" "ecr" {
+#   name        = "${aws_iam_user.cd.name}-ecr"
+#   description = "Allow user to manage ECR resources"
+#   policy      = data.aws_iam_policy_document.ecr.json
+# }
+
+# resource "aws_iam_user_policy_attachment" "ecr" {
+#   user       = aws_iam_user.cd.name
+#   policy_arn = aws_iam_policy.ecr.arn
+# }
+
+#########################
+# Policy for ECS access #
+#########################
+
+data "aws_iam_policy_document" "ecs" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecs:DescribeClusters",
+      "ecs:DeregisterTaskDefinition",
+      "ecs:DeleteCluster",
+      "ecs:DescribeServices",
+      "ecs:UpdateService",
+      "ecs:DeleteService",
+      "ecs:DescribeTaskDefinition",
+      "ecs:CreateService",
+      "ecs:RegisterTaskDefinition",
+      "ecs:CreateCluster",
+      "ecs:UpdateCluster",
+      "ecs:TagResource",
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "ecs" {
+  name        = "${aws_iam_user.cd.name}-ecs"
+  description = "Allow user to manage ECS resources."
+  policy      = data.aws_iam_policy_document.ecs.json
+}
+
+resource "aws_iam_user_policy_attachment" "ecs" {
+  user       = aws_iam_user.cd.name
+  policy_arn = aws_iam_policy.ecs.arn
+}
+
+#########################
+# Policy for IAM access #
+#########################
+
+data "aws_iam_policy_document" "iam" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "iam:ListInstanceProfilesForRole",
+      "iam:ListAttachedRolePolicies",
+      "iam:DeleteRole",
+      "iam:ListPolicyVersions",
+      "iam:DeletePolicy",
+      "iam:DetachRolePolicy",
+      "iam:ListRolePolicies",
+      "iam:GetRole",
+      "iam:GetPolicyVersion",
+      "iam:GetPolicy",
+      "iam:CreateRole",
+      "iam:CreatePolicy",
+      "iam:AttachRolePolicy",
+      "iam:TagRole",
+      "iam:TagPolicy",
+      "iam:PassRole"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "iam" {
+  name        = "${aws_iam_user.cd.name}-iam"
+  description = "Allow user to manage IAM resources."
+  policy      = data.aws_iam_policy_document.iam.json
+}
+
+resource "aws_iam_user_policy_attachment" "iam" {
+  user       = aws_iam_user.cd.name
+  policy_arn = aws_iam_policy.iam.arn
+}
+
+################################
+# Policy for CloudWatch access #
+################################
+
+data "aws_iam_policy_document" "logs" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:DeleteLogGroup",
+      "logs:DescribeLogGroups",
+      "logs:CreateLogGroup",
+      "logs:TagResource",
+      "logs:ListTagsLogGroup"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "logs" {
+  name        = "${aws_iam_user.cd.name}-logs"
+  description = "Allow user to manage CloudWatch resources."
+  policy      = data.aws_iam_policy_document.logs.json
+}
+
+resource "aws_iam_user_policy_attachment" "logs" {
+  user       = aws_iam_user.cd.name
+  policy_arn = aws_iam_policy.logs.arn
+}
